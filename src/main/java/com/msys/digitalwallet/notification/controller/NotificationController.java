83a442f2759141f82429ff2ff35893cc385e0573
Twilio-POC <tprabu@msystechnologies.com>
2023-05-22 11:47:36 +0530

    NotificationService - Cors config

--- src/main/java/com/msys/digitalwallet/notification/controller/NotificationController.java
+++ src/main/java/com/msys/digitalwallet/notification/controller/NotificationController.java
@@ -7,14 +7,12 @@ import com.msys.digitalwallet.notification.service.NotificationService;
 import io.micrometer.core.annotation.Timed;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/notification")
 @Timed
+@CrossOrigin(maxAge = 3600)
 public class NotificationController{
 
     @Autowired
